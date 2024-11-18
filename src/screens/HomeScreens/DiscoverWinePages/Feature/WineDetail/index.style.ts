7a12f039fe4fa11c33c3015e310474be07a5efbb
Tushant8853 <tushantgupta8853@gmail.com>
2024-11-18 15:00:04 +0530

    Wine Details Page Complete backend and all

---
+++ src/screens/HomeScreens/DiscoverWinePages/Feature/WineDetail/index.style.ts
@@ -0,0 +1,164 @@
+// WineDetailsStyles.ts
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    marginBottom: 100,
+    paddingTop: 47,
+  },
+  headerContainer: {
+    height: 40,
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    justifyContent: 'space-between',
+  },
+  iconContainer: {
+    marginHorizontal: 2,
+  },
+  spacer: {
+    flex: 1,
+  },
+  SubContainer: {
+    flexDirection: 'row',
+  },
+  ImgContainer: {
+    height: '100%',
+    width: 90,
+    overflow: 'hidden',
+  },
+  image: {
+    height: '95%',
+    width: '200%',
+    marginLeft: '-100%',
+  },
+  ScrollViewContainer: {
+    marginTop:-50,
+    paddingVertical:'100%',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  detailsContainer: {
+    flex: 0.6,
+    paddingLeft: 16,
+    justifyContent: 'center',
+  },
+  wineryNameContainer: {
+    marginBottom: 4,
+  },
+  wineryName: {
+    fontFamily: 'Hiragino Kaku Gothic Pro',
+    fontSize: 20,
+    fontWeight: '600',
+    lineHeight: 30,
+    textDecorationStyle: 'solid',
+    textDecorationColor: 'black',
+    color: '#522F60',
+  },
+  wineryNameJapanese: {
+    fontFamily: 'Hiragino Kaku Gothic Pro',
+    fontSize: 10,
+    fontWeight: '300',
+    lineHeight: 15,
+    textDecorationStyle: 'solid',
+    textDecorationColor: 'black',
+    color: '#522F60',
+  },
+  wineNameContainer: {
+    marginBottom: 4,
+  },
+  wineName: {
+    fontFamily: 'Hiragino Kaku Gothic Pro', 
+    fontSize: 16,
+    fontWeight: '600',
+    lineHeight: 24,
+    textDecorationStyle: 'solid',
+    textDecorationColor: 'black',
+    color: '#522F60',
+  },
+  wineNameJapanese: {
+    fontFamily: 'Hiragino Kaku Gothic Pro',
+    fontSize: 10,
+    fontWeight: '300',
+    lineHeight: 15,
+    textDecorationStyle: 'solid',
+    textDecorationColor: 'black',
+    color: '#522F60',
+  },
+  vintageContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 4,
+  },
+  vintage: {
+    fontFamily: 'SF Pro',
+    fontSize: 14,
+    fontWeight: '500',
+    lineHeight: 16.71,
+    textDecorationStyle: 'solid',
+    color: '#522F60',
+    marginRight: 16,
+  },
+  vintageJapanese: {
+    fontFamily: 'SF Pro',
+    fontSize: 10,
+    fontWeight: '300',
+    lineHeight: 11.93,
+    textDecorationStyle: 'solid',
+    color: '#522F60',
+  },
+  wineTypeConatiner: {
+    marginBottom: 4,
+  },
+  wineType: {
+    fontFamily: 'Hiragino Kaku Gothic Pro', 
+    fontSize: 14,
+    fontWeight: '300',
+    lineHeight: 21,
+    textDecorationStyle: 'solid',
+    textDecorationColor: 'black',
+    color: '#522F60',
+  },
+  starsContainer: {
+    marginBottom: 4,
+  },
+  stars: {
+    fontSize: 18,
+    color: '#4B0082',
+    marginBottom: 4,
+  },
+  ratingsContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  ratingBox: {
+    width: 44,
+    height: 16,
+    backgroundColor: '#4B0082',
+    borderRadius: 4,
+    marginRight: 2,
+    justifyContent:'center',
+    alignItems:'center',
+  },
+  ratingText:{
+    fontFamily: 'SF Pro',
+    fontSize: 10,
+    color: 'white',
+    fontWeight:'500',
+  },
+  ScrollDiscriptionContainer:{
+    marginLeft: 40,
+    marginRight:8,
+  },
+  bottom:{
+    marginBottom:100,
+  },
+  
+});
+
+export default styles;
