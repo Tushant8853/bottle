e4c7d62ebfec304a90ca813a48f78c64a1666676
Moin-khan21 <moinhabibkhang21@gmail.com>
2024-12-17 15:11:34 +0530

    camera module removed

--- src/screens/HomeMe/MeDashboard.tsx
+++ src/screens/HomeMe/MeDashboard.tsx
@@ -1,31 +1,12 @@
-import { AntDesign } from '@expo/vector-icons';
-import { CameraType, CameraView, useCameraPermissions } from 'expo-camera';
-import { useRef, useState } from 'react';
+
 import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import * as Sentry from '@sentry/react-native';
+
 
 export default function Camera() {
-  const [facing, setFacing] = useState<CameraType>('back');
-  const [permission, requestPermission] = useCameraPermissions();
-  const [photo, setPhoto] = useState<any>(null);
-  const cameraRef = useRef<CameraView | null>(null);
-  if (!permission) {
-    // Camera permissions are still loading.
-    return <View />;
-  }
-  if (!permission.granted) {
-    // Camera permissions are not granted yet.
-    return (
-      <View style={styles.container}>
-        <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
-        <Button onPress={requestPermission} title="grant permission" />
-      </View>
-    );
-  }
+  
   return (
     <View style={styles.container}>
-      <CameraView style={styles.camera} facing={facing} ref={cameraRef}>
-      </CameraView>
+      <Text>me page</Text>
     </View>
   );
 }
