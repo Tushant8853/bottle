c04e4b132354d7e5ac6732d5c01841ec65a9cc66
Tushant8853 <tushantgupta8853@gmail.com>
2024-12-19 10:54:38 +0530

    Update

--- src/screens/HomeMe/MeDashboard.tsx
+++ src/screens/HomeMe/MeDashboard.tsx
@@ -6,7 +6,7 @@ export default function Camera() {
   
   return (
     <View style={styles.container}>
-      <Text>me page</Text>
+      <Text>Camera Coming Soon</Text>
     </View>
   );
 }
@@ -15,28 +15,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-  },
-  camera: {
-    flex: 1,
-  },
-  buttonContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    backgroundColor: 'transparent',
-    margin: 64,
-  },
-  button: {
-    marginBottom:100,
-    flex: 1,
-    alignSelf: 'flex-end',
     alignItems: 'center',
-    marginHorizontal: 10,
-    backgroundColor: 'gray',
-    borderRadius: 10,
-  },
-  text: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: 'white',
+    backgroundColor: 'white',
   },
 });
